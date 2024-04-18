0fdfb613c5e5cd6518a5684fe0808a205d660903
akramalex <info@quick4you.net>
2024-04-18 17:22:58 +0100

    done

--- reduce.js
+++ reduce.js
@@ -7,6 +7,44 @@
 
 // Summing an array of numbers:
 
+const nums =[0, 1, 2, 3, 4]
+let sum = nums.reduce((acc, curr) => acc + curr, 0);
+
+console.log(sum)
+
+// explain the acc and curr //
+
+const ne = [5, 6, 1, 0];
+
+let sum2 = ne.reduce ((acc, curr) => {
+  console.log(
+    " Accumulator:", acc,
+    "current value:", curr, 
+    "total:", acc + curr
+  );
+  return acc + curr;
+});
+
+console.log(sum2);
+
+
+// another example to add extra  number //
+
+const me = [5, 6, 1, 0];
+
+let sum3 = me.reduce ((acc, curr) => {
+  console.log(
+    " Accumulator:", acc,
+    "current value:", curr, 
+    "total:", acc + curr
+  );
+  return acc + curr;
+}, 6);
+
+console.log(sum3);
+
+
+
 
 const teamMembers = [
   {
@@ -34,4 +72,38 @@ const teamMembers = [
 // Totaling a specific object property
 
 
+
+let totalExper = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0);
+
+console.log(totalExper)
+
+
+// see without see example 
+// this bugs comes out   [object Object]713 //
+
+let totalExpe = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience);
+
+console.log(totalExpe)
+
+
+
 // Grouping by a property, and totaling it too
+
+// we want this result //
+// developer :12 desinger :4 //
+
+let expbypro =teamMembers.reduce((acc, curr) => {
+  let key= curr.profession;
+  if (!acc[key]) {
+    acc[key] = curr.yrsExperience;
+  } else {
+    acc[key] += curr.yrsExperience;
+  }
+
+  return acc;
+
+} , {});
+
+console.log(expbypro)
+
+
